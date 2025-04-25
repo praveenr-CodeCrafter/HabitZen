56af7515367bc8029feeb9d6617f35f405ca9ae4
Praveen10 <praveenrajamani58@gmail.com>
2025-04-25 17:01:30 +0100

    working on reset password backend

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,5 +4,5 @@ from .views import RegisterView, LoginView,  ResetPasswordView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    # path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
 ]
